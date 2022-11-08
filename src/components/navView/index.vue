dc84396088a2696eba4a6d68073adb4ed3cef6a1
Chai chai <2787922490@qq.com>
2022-11-08 23:34:32 +0800

    框架搭建，完成首页

---
+++ src/components/navView/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <nav class="navBar">
+    <div class="logoBox">
+      <img src="@/assets/4433white.png" alt="4433工作室logo" class="logo" />
+    </div>
+
+    <div>
+      <el-button round size="mini" class="loginBtn">登录</el-button>
+      <el-button round size="mini" class="regiestBtn">注册</el-button>
+    </div>
+
+    <el-menu
+      :default-active="activeIndex"
+      class="el-menu-demo"
+      mode="horizontal"
+      background-color="#000"
+      text-color="#fff"
+      active-text-color="rgb(220,223,230)"
+      @select="handleSelect"
+    >
+      <el-menu-item index="1">首页</el-menu-item>
+      <el-menu-item index="2">团队介绍</el-menu-item>
+      <el-menu-item index="3">作品展示</el-menu-item>
+      <el-menu-item index="4">加入我们</el-menu-item>
+      <el-menu-item index="5">联系我们</el-menu-item>
+    </el-menu>
+  </nav>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: "1",
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.navBar {
+  width: 100vw;
+  display: flex;
+  flex-wrap: nowrap;
+  margin: 0 auto;
+  justify-content: space-between;
+  .logoBox {
+    width: 250px;
+    position: absolute;
+    .logo {
+      width: 250px;
+      margin-left: 30px;
+      margin-top: 10px;
+    }
+  }
+  .loginBtn {
+    position: relative;
+    left: 310px;
+    top: 35px;
+    color: #fff;
+    background: rgba($color: #000000, $alpha: 0.3);
+  }
+  .regiestBtn {
+    position: relative;
+    left: 320px;
+    top: 35px;
+    color: #fff;
+    background: rgba($color: #000000, $alpha: 0.3);
+  }
+}
+</style>
+
+<style lang="scss">
+.el-menu.el-menu--horizontal {
+  border-bottom: 0 !important;
+}
+.el-menu-demo {
+  margin-right: 30px;
+  margin-top: 10px;
+}
+</style>
---
+++ src/views/homeView/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="homeBg">
+    <div class="ContentBox">
+      <h1 class="title">4433工作室</h1>
+      <p class="content">谁说网页就是平平无奇？看看00后会有怎样的奇思妙想🤨</p>
+    </div>
+    <div>
+      <img src="@/assets/bg.jpg" alt="" class="bgimg" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.homeBg {
+  position: absolute;
+  overflow: hidden;
+  .ContentBox {
+    margin-top: 320px;
+    margin-left: 300px;
+    .title {
+      font-size: 50px;
+      letter-spacing: 2px;
+    }
+    .content {
+      margin-top: 10px;
+      font-size: 20px;
+      letter-spacing: 1px;
+    }
+  }
+  .bgimg {
+    position: fixed;
+    right: 300px;
+    top: 400px;
+    width: 300px;
+    display: inline-block;
+    filter: Alpha(opacity=50);
+    opacity: 0.5;
+    filter: none;
+  }
+}
+</style>
