e8279a604605720bfe76d0e2f1f44443b00dbd81
moqing <jchz_cn@163.com>
2023-02-09 21:02:23 +0800

    新增用户设备判断,手机端警告页面

---
+++ src/components/user-warningView/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="mobile">
+    <div class="mobileTitle">😵移动端暂不支持</div>
+    <div class="mobileContent">
+      请使用电脑访问
+      <p>
+        <a href="http://chaichaisocute.top" class="mobileHref"
+          >http://chaichaisocute.top</a
+        >
+      </p>
+    </div>
+  </div>
+</template>
+
+<script></script>
+
+<style>
+.mobile {
+  width: 100%;
+  height: 100vh;
+  background: #000;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+.mobileTitle {
+  font-size: 25px;
+  padding: 5px;
+  border-radius: 25px;
+  border-width: 3px;
+  border-style: solid;
+}
+.mobileContent {
+  display: flex;
+  font-size: 20px;
+  margin-top: 20px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+.mobileHref {
+  color: #57eae8;
+  text-decoration: none;
+}
+</style>
